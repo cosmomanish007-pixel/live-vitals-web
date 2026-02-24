7ea6aec3ac318b3249d6c7187096d2c7b1fcca07
cosmomanish007-pixel <cosmomanish007@gmail.com>
2026-02-24 15:01:57 +0530

    MAIN

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -178,7 +178,7 @@ useEffect(() => {
   fetchAllPrescriptionData();
 }, [session]);
 
-A/* ===============================
+/* ===============================
    REALTIME VIDEO LISTENER
 ================================= */
 
